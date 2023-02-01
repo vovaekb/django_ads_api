2f0e1051d955c6b83841b872d633be582e02717f
Vladimir Privalov <vovaprivalov@gmail.com>
2023-02-02 00:28:22 +0400

    Add User model

--- ads_django_app/settings.py
+++ ads_django_app/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'ads'
 ]
 
