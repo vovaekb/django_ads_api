fa9c18bfe3d687d18c38b71f63017a348691db16
Vladimir Privalov <vovaprivalov@gmail.com>
2023-02-03 23:51:42 +0400

    Add model Ad

--- ads/models.py
+++ ads/models.py
@@ -3,3 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Ad(models.Model):
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=350)
+    status = models.CharField(max_length=15)
