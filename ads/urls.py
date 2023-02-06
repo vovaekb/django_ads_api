0c7da2ee43cc38067940c1476d2009974bee9b09
Vladimir Privalov <vovaprivalov@gmail.com>
2023-02-06 07:30:28 +0400

    Fix urls

--- ads/urls.py
+++ ads/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from ads.views import ListAdsView
 
 urlpatterns = [
-    path('ads/', ListAdsView.as_view(), name='ads-all')
+    path('list', ListAdsView.as_view(), name='ads-all')
 ]
