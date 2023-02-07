ce458b2576cbd523d70b90ef935a5333409789df
Vladimir Privalov <vovaprivalov@gmail.com>
2023-02-07 19:14:58 +0400

    Add superuser and test case for him

--- ads/tests.py
+++ ads/tests.py
@@ -13,11 +13,23 @@ class TestAds(APITestCase):
         self.payd_data = {'status': 'оплачено', 'ids': self.ids}
         self.ads = []
         self.user = User.objects.create_user('test_user_1', 'example@test.com', '123')
+        self.superuser = User.objects.create_superuser('admin', 'example@test.com', '123')
+        # create a set of test ads
         for i in range(TEST_SIZE):
             self.ads.append(Ad.objects.create(title='Test ad {}'.format(i), category='разное', status='отказ', author=self.user)) 
 
     def test_update_ads(self):
         # Invalid case
+        response = self.client.put(
+            'ads/status',
+            data=json.dumps(self.payd_data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Correct case = superuser
+        self.logged_in = self.client.login(
+            username='admin', password='123'
+        )
         response = self.client.put(
             'ads/status',
             data=json.dumps(self.payd_data),
